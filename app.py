dda938a6cd4c4ed71d4767e7cf8cc39e5683ffb9
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-05-17 19:47:14 +0000

    Add login functionallity(display validation errors & add flash msg when user logged in

--- app.py
+++ app.py
@@ -45,6 +45,13 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'Test' and form.password.data == 'fakepasswrd':
+            flash('You have successfully logged-in \u2713', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Username or password is incorrect. Please try again ',
+                  'danger')
     return render_template("login.html", title='Login', form=form)
 
 
