2e4ab2f0d592ad001e1ae75fdf27feea0fa4c307
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-05-18 13:34:43 +0000

    Add Regexp validation for username input

--- forms.py
+++ forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
 class RegisterForm(FlaskForm):
     username = StringField(
-        'Username', validators=[DataRequired(), Length(min=2, max=15)])
+        'Username', validators=[DataRequired(), Length(min=2, max=15),
+                                Regexp('^([a-zA-Z0-9-!@#$%^&*])',
+                                message="Username can't start with space")])
     password = PasswordField(
         'Password', validators=[DataRequired(), Length(min=8, max=30)])
     confirm_password = PasswordField('Confirm Password',
@@ -17,7 +19,9 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField(
-        'Username', validators=[DataRequired(), Length(min=2, max=15)])
+        'Username', validators=[DataRequired(), Length(min=2, max=15),
+                                Regexp('^([a-zA-Z0-9-!@#$%^&*])',
+                                message="Username can't start with space")])
     password = PasswordField(
         'Password', validators=[DataRequired(), Length(min=8, max=30)])
     submit = SubmitField('Login')
