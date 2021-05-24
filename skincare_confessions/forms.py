9cd7a652800d71122e02f6135209c6eff404554f
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-05-24 20:26:39 +0000

    Initial build of Add reviewform fields additional TextAreaField imported

--- skincare_confessions/forms.py
+++ skincare_confessions/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import (StringField, PasswordField, SubmitField,
+                     TextAreaField)
 from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
@@ -25,3 +26,17 @@ class LoginForm(FlaskForm):
     password = PasswordField(
         'Password', validators=[DataRequired(), Length(min=8, max=30)])
     submit = SubmitField('Login')
+
+
+class AddReviewForm(FlaskForm):
+    brand_name = StringField(
+            'Brand Name', validators=[DataRequired(), Length(min=1, max=30),
+                                      Regexp('^([a-zA-Z0-9-!@#$%^&*])',
+                                      message="Brand name can't start with"
+                                      "space")])
+    product_review = TextAreaField(
+            'Product Review', validators=[DataRequired(),
+                                          Length(min=10, max=2000),
+                                          Regexp('^([a-zA-Z0-9-!@#$%^&*])',
+                                          message="Product review can't start"
+                                          "with space")])
