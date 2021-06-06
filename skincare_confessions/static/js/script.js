14e2212838fb4154c76fafc8305c958fe9fcc771
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-06-06 22:06:42 +0000

    Add eventlistener to determine state of checkbox, toggle switch according. Hide form checkbox

--- skincare_confessions/static/js/script.js
+++ skincare_confessions/static/js/script.js
@@ -1,3 +1,9 @@
-            $('#sidebarCollapse').click( function () {
-                $('#sidebar').toggleClass('active');
-            });
+$('#sidebarCollapse').click( function () {
+    $('#sidebar').toggleClass('active');
+});
+
+/* Listens to change event on is_vegan boolean checkboxfield, marks true when checked */
+$('#is_vegan').on('change', (e) => {
+  this.checkbox = e.target.checked ? 'true' : 'false';
+})
+
