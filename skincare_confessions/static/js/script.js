542fe9921641863a83b213786acf70c726367aa0
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-06-07 17:33:36 +0000

    Star rating color styling functionallity added,bug detected

--- skincare_confessions/static/js/script.js
+++ skincare_confessions/static/js/script.js
@@ -1,9 +1,32 @@
-$('#sidebarCollapse').click( function () {
-    $('#sidebar').toggleClass('active');
+/* Sidebar navigation */
+$("#sidebarCollapse").click(function () {
+    $("#sidebar").toggleClass("active");
 });
 
 /* Listens to change event on is_vegan boolean checkboxfield, marks true when checked */
-$('#is_vegan').on('change', (e) => {
-  this.checkbox = e.target.checked ? 'true' : 'false';
-})
+$("#is_vegan").on("change", (e) => {
+    this.checkbox = e.target.checked ? "true" : "false";
+});
 
+/* Star Rating radiofield*/
+
+$("#rating-0").click(function () {
+    $(".fa-star").css("color", "#cccccc");
+    $("#rating-0").css("color", "#ffd01f");
+});
+$("#rating-1").click(function () {
+    $(".fa-star").css("color", "#cccccc");
+    $("#rating-0, #rating-1").css("color", "#ffd01f");
+});
+$("#rating-2").click(function () {
+    $(".fa-star").css("color", "#cccccc");
+    $("#rating-0, #rating-1, #rating-2").css("color", "#ffd01f");
+});
+$("#rating-3").click(function () {
+    $(".fa-star").css("color", "#cccccc");
+    $("#rating-0, #rating-1, #rating-2, #rating-3").css("color", "#ffd01f");
+});
+$("#rating-4").click(function () {
+    $(".fa-star").css("color", "#cccccc");
+    $("#rating-0, #rating-1, #rating-2, #rating-3, #rating-4").css("color", "#ffd01f");
+});
