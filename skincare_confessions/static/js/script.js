432ea21c8bd955135376e9b7facab9633a3aa095
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-07-04 21:42:11 +0000

    Remove wrapper,add close btn to sidebar & overlay to cover page when collapsed

--- skincare_confessions/static/js/script.js
+++ skincare_confessions/static/js/script.js
@@ -1,7 +1,15 @@
 /* Sidebar navigation */
 $("#sidebarCollapse").click(function () {
     $("#sidebar").toggleClass("active");
+     // Display overlay
+    $('.overlay').addClass('active');
 });
+ $('#dismiss-btn, .overlay').on('click', function () {
+            // Hide sidebar
+            $('#sidebar').removeClass('active');
+            // Hide overlay
+            $('.overlay').removeClass('active');
+        });
 
 /* Listens to change event on is_vegan boolean checkboxfield, marks true when checked */
 $("#is_vegan").on("change", (e) => {
