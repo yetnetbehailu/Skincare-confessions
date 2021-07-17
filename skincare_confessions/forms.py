6a5097696f8287c41d2b69d96768b15d9aa69e06
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-07-17 23:00:55 +0000

    InputRequired validator replace DataRequired.Brand charc lenght changed

--- skincare_confessions/forms.py
+++ skincare_confessions/forms.py
@@ -2,20 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, SubmitField,
                      TextAreaField, BooleanField,
                      DecimalField, RadioField)
-from wtforms.validators import (DataRequired, Length, EqualTo, Regexp,
+from wtforms.validators import (InputRequired, Length, EqualTo, Regexp,
                                 Email)
 from flask_wtf.file import FileField, FileAllowed
 
 
 class RegisterForm(FlaskForm):
     username = StringField(
-        'Username', validators=[DataRequired(), Length(min=2, max=15),
+        'Username', validators=[InputRequired(), Length(min=2, max=15),
                                 Regexp('^([a-zA-Z0-9-!@#$%^&*])',
                                 message="Username can't start with space")])
     password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=8, max=30)])
+        'Password', validators=[InputRequired(), Length(min=8, max=30)])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(),
+                                     validators=[InputRequired(),
                                                  Length(min=8, max=30),
                                                  EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -23,46 +23,43 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField(
-        'Username', validators=[DataRequired(), Length(min=2, max=15),
+        'Username', validators=[InputRequired(), Length(min=2, max=15),
                                 Regexp('^([a-zA-Z0-9-!@#$%^&*])',
                                 message="Username can't start with space")])
     password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=8, max=30)])
+        'Password', validators=[InputRequired(), Length(min=8, max=30)])
     submit = SubmitField('Login')
 
 
 class AddReviewForm(FlaskForm):
     brand_name = StringField(
-            'Brand Name', validators=[DataRequired(), Length(min=1, max=30),
+            'Brand Name', validators=[InputRequired(), Length(min=1, max=15),
                                       Regexp('^([a-zA-Z0-9-!@#$%^&*])',
                                       message="Brand name can't start with"
                                       "space")])
     product_review = TextAreaField(
             'Product Review',
-            validators=[DataRequired(), Length(min=10, max=2000),
+            validators=[InputRequired(), Length(min=10, max=2000),
                         Regexp('^([a-zA-Z0-9-!@#$%^&*])',
                         message="Product review can't start with space")])
-    price = DecimalField('Price', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[InputRequired()])
     is_vegan = BooleanField('Vegan')
-    rating = RadioField('Rating', validators=[DataRequired()],
+    rating = RadioField('Rating', validators=[InputRequired()],
                         choices=[('1', 'Boycott'),
                                  ('2', 'Poor'),
                                  ('3', 'Good'),
                                  ('4', 'Very Good'),
                                  ('5', 'Fantastic')])
     tags = StringField(
-        'Search tag', validators=[DataRequired(), Length(min=1, max=30),
-                                  Regexp('^([a-zA-Z0-9-!@#$%^&*])',
-                                  message="Search tag can't start with space")
-                                  ])
-    upload_img = FileField(
-        'Upload image',
-        validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+        'Tag', validators=[InputRequired(), Length(min=1, max=30),
+                           Regexp('^([a-zA-Z0-9-!@#$%^&*])',
+                           message="Search tag can't start with space")])
+    upload_img = FileField(validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Add')
     cancel = SubmitField('Cancel')
 
 
 class EmailForm(FlaskForm):
     email = StringField(
-        'Enter Email...', validators=[DataRequired(), Email()])
+        'Enter Email...', validators=[InputRequired(), Email()])
     subscribe = SubmitField('Sign up!')
