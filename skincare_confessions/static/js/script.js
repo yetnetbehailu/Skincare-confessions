85f522f52b21a68bc23194362aa72690e99312c3
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-08-02 16:39:20 +0000

    Syntax corrections made

--- skincare_confessions/static/js/script.js
+++ skincare_confessions/static/js/script.js
@@ -2,18 +2,19 @@
 $("#sidebarCollapse").click(function () {
     $("#sidebar").toggleClass("active");
      // Display overlay
-    $('.overlay').addClass('active');
+    $(".overlay").addClass("active");
 });
- $('#dismiss-btn, .overlay').on('click', function () {
+ $("#dismiss-btn, .overlay").on("click", function () {
             // Hide sidebar
-            $('#sidebar').removeClass('active');
+            $("#sidebar").removeClass("active");
             // Hide overlay
-            $('.overlay').removeClass('active');
+            $(".overlay").removeClass("active");
         });
 
-/* Listens to change event on is_vegan boolean checkboxfield, marks true when checked */
+/* Listens to change event on is_vegan boolean checkboxfield,
+marks true when checked */
 $("#is_vegan").on("change", function (e) {
-    this.checkbox = e.target.checked ? "true" : "false";
+    this.checkbox = (e.target.checked ? "true" : "false");
 });
 
 /* Star Rating radiofield*/
@@ -32,9 +33,11 @@ $("#rating-2").click(function () {
 });
 $("#rating-3").click(function () {
     $(".fa-star").css("color", "#cccccc");
-    $("#rating-0, #rating-1, #rating-2, #rating-3").css("color", "#ffd01f");
+    $("#rating-0, #rating-1, #rating-2, #rating-3").css(
+        "color", "#ffd01f");
 });
 $("#rating-4").click(function () {
     $(".fa-star").css("color", "#cccccc");
-    $("#rating-0, #rating-1, #rating-2, #rating-3, #rating-4").css("color", "#ffd01f");
+    $("#rating-0, #rating-1, #rating-2, #rating-3, #rating-4").css(
+        "color","#ffd01f");
 });
